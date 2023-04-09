456105af18c736adda91a73139277dc379b2b7f6
nuttapat115 <nuttapat115_@hotmail.com>
2023-04-09 23:19:55 +0700

    Added user profile feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -37,3 +37,20 @@ class UserProfileSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         return super().update(instance, validated_data)
+
+# class ProfileFeedItemSerializer(serializers.ModelSerializer):
+#     '''Serializer profile feed item serializer'''
+
+#     class Meta:
+#         model = models.ProfileFeedItem
+#         fields = ('id', 'user_profile', 'status_text', 'create_on')
+#         extra_kwargs = {'user_profile':{'read_only':True}}
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
@@ -107,3 +108,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     ''' Handle creating user authentication tokens'''
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewset(viewsets.ModelViewSet):
+    ''' Handle creating, reading and updating user profile feed items'''
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializers):
+        '''Sets the user profile to logged in user'''
+        serializers.save(user_profile= self.request.user)
